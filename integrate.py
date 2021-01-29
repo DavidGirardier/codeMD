dcb98e063065fa925f853821f191e13bedcf5603
David Girardier <David>
2021-01-29 17:12:26 +0100

    More flexible scripts

--- integrate.py
+++ integrate.py
@@ -1,6 +1,7 @@
 from numpy import sqrt, array, correlate, arange, mean, loadtxt, savetxt, c_, split, swapaxes, trapz
 
-file = open('nomean_Na.out', 'r')
+inputfile = input("Vacf file: ")
+file = open(inputfile, 'r')
 Lines = file.readlines()
 counter = -1
 dt=[]
@@ -8,10 +9,11 @@ xx=[]
 yy=[]
 zz=[]
 xy=[]
+Nt=2000
 for line in Lines:
 
     counter = counter + 1
-    if (counter < 1000) :
+    if (counter < Nt) :
         splitted = line.split()
         splitted = [float(i) for i in splitted] #split each line
 
@@ -19,8 +21,8 @@ for line in Lines:
         xx.append(splitted[1])
         yy.append(splitted[2])
         zz.append(splitted[3])
-        xy.append(splitted[4])
-    if (counter==1000):
+        # xy.append(splitted[4])
+    if (counter==Nt):
         break
         counter = -1
 
@@ -28,23 +30,30 @@ dt = array(dt)
 xx = array(xx)
 yy = array(yy)
 zz = array(zz)
-xy = array(xy)
+#xy = array(xy)
 
 
 Dxx = []
 Dyy = []
 Dzz = []
 Dxy = []
-for t in range(1000):
+sum=0.0
+Integral = []
+for t in range(Nt):
+    print(xx[t])
     Dxx.append(trapz(xx[:t+1],dt[:t+1]))
     Dyy.append(trapz(yy[:t+1],dt[:t+1]))
     Dzz.append(trapz(zz[:t+1],dt[:t+1]))
-    Dxy.append(trapz(xy[:t+1],dt[:t+1]))
-
+    # Dxy.append(trapz(xy[:t+1],dt[:t+1]))
+#     d=(xx[t+1]+xx[t])/2*dt[1]
+#     sum=sum+d
+#     print(d)
+#     Integral.append(sum)
+#
+# Integral = array(Integral)
 Dxx = array(Dxx)
 Dyy = array(Dyy)
 Dzz = array(Dzz)
-Dxy = array(Dxy)
-
-
-savetxt('diffusionnommean_Na.out', c_[Dxx,Dyy,Dzz,Dxy])
+# Dxy = array(Dxy)
+outputfile = 'diffusion_' + inputfile
+savetxt(outputfile, c_[dt,Dxx,Dyy,Dzz])
