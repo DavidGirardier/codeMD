6d1fcfe7c4841c17ba7e58547ce326c6f228ff8b
David <david.girardier@epfl.ch>
2021-04-19 18:01:40 +0200

    many progs

--- estimator.py
+++ estimator.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-totfile = open('tot.txt', 'r')
-potfile = open('pot.txt', 'r')
+file = open("FullAnalysis.txt","r")
 
 
-totLines = totfile.readlines()
-potLines = potfile.readlines()
+
+fileLines = file.readlines()
 
 sum_tot=0.0
 sum_tot2=0.0
@@ -17,37 +16,37 @@ sum_pot2=0.0
 avg_pot = 0.0
 avg_pot2 = 0.0
 
+sum_kin=0.0
+sum_kin2=0.0
+avg_kin = 0.0
+avg_kin2 = 0.0
+
 count= 0
-for line in totLines:
+for line in fileLines:
     count = count + 1
     #print(line)
     #print line.split(" ")
     splitted = line.split()
     splitted = [float(i) for i in splitted]
     #print(splitted[0])
-    sum_tot = sum_tot + splitted[0]
-    sum_tot2 = sum_tot2 + splitted[0]**2
+    sum_tot = sum_tot + splitted[1]
+    sum_tot2 = sum_tot2 + splitted[1]**2
+    sum_kin = sum_kin + splitted[2]
+    sum_kin2 = sum_kin2 + splitted[2]**2
+    sum_pot = sum_pot + splitted[3]
+    sum_pot2 = sum_pot2 + splitted[3]**2
     #print(str(sum_tot)+ '\t')
     #print(str(sum_tot2) + '\n')
 
-for line in potLines:
 
-    #print(line)
-    #print line.split(" ")
-    splitted = line.split()
-    splitted = [float(i) for i in splitted]
-    #print(splitted[0])
-    sum_pot = sum_pot + splitted[0]
-    sum_pot2 = sum_pot2 + splitted[0]**2
-    #print(str(sum_tot)+ '\t')
-    #print(str(sum_tot2) + '\n')
-
-#print(str(sum_tot) + '  ' + str(sum_tot2))
 avg_tot = sum_tot/count
 avg_tot2 = sum_tot2/count
 
 DE = abs((avg_tot2 - avg_tot**2))**0.5
-print(str(DE))
+
+avg_kin = sum_kin/count
+avg_kin2 = sum_kin2/count
+DK = abs((avg_kin2 - avg_kin**2))**0.5
 #print('\n')
 avg_pot = sum_pot/count
 avg_pot2 = sum_pot2/count
@@ -55,4 +54,4 @@ avg_pot2 = sum_pot2/count
 DV = abs((avg_pot2 - avg_pot**2))**0.5
 
 
-print(str(DE/DV))
+print("DE/DV = " + str(DE/DV) + "\t DE = " + str(DE) + "\t DK = " + str(DK) + "\t DV = " + str(DV))
